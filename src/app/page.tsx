ee69132e042be8180bde4c07d36f304665fbdbe2
vidhanshu <vidhanshu7218@gmail.com>
2024-09-06 00:45:26 +0530

    chore: setup convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@/components/ui/button";
 import AuthScreen from "@/features/auth/components/AuthScreen";
-import { AuthFlow } from "@/features/auth/types";
-import React, { useState } from "react";
+import React from "react";
 
 const Home = () => {
   return <AuthScreen/>
